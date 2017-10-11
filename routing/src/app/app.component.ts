ccdcd2bb733b8fd1142cd4c188ed1a3a3a23444d
m.kolasinski <m.kolasinski@poczta.fm>
2017-10-11 17:47:17 +0200

    - adding imports

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Componen, OnInit } from '@angular/core';
 import { dia, shapes } from 'jointjs';
+import * as $ from 'jquery';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
